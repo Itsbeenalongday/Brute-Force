cfe1e5a4e60be79a54efdbaffa79afe223b416a9
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-02-13 22:53:21 +0900

    second commit

---
+++ 1476_날짜 계산/날짜 계산.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int E, S, M;
+	cin >> E >> S >> M;
+
+	return 0;
+}
